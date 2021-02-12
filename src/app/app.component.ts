9b91ca6b7ef4a06e128c302b3627aee0bc6fb8df
twolak88 <twolak88@gmail.com>
2021-02-13 00:07:20 +0100

    assignment solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  suggestUserName() {
-    const suggestedName = 'Superuser';
+  @ViewChild('formEl', {static: false}) signupForm: NgForm;
+  defaultSubscription = 'Advanced';
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  authData = {
+    email: '',
+    subscription: '',
+    password: ''
+  }
+  submitted = false;
+
+  onSubmit() {
+    console.log(this.signupForm.value);
+    this.submitted = true;
+    this.authData.email = this.signupForm.value.email;
+    this.authData.subscription = this.signupForm.value.subscription;
+    this.authData.password = this.signupForm.value.password;
   }
 }
