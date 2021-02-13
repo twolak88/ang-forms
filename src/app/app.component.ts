109cabf090eb6bb5ff02ffe409af66d2ebf1ae40
twolak88 <twolak88@gmail.com>
2021-02-13 10:40:18 +0100

    custom validation; errors handling in form; displaying error messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,12 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       'userData' : new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male'),
@@ -32,5 +33,12 @@ export class AppComponent implements OnInit{
 
   get controls() {
     return (this.signupForm.get('hobbies') as FormArray).controls;
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if(this.forbiddenUsernames.includes(control.value)) {
+      return {'nameIsForbidden': true};
+    }
+    return null;//null if control is valid
   }
 }
